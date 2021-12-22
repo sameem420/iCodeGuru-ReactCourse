4a9d57918cf74191bd4a71f28e9a56a1abbe90c9
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2021-12-22 22:21:42 +0500

    react router code added.

---
+++ folio3bootcamp-ReactCourse/week3day3code/src/App.js
@@ -0,0 +1,51 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Route, Routes, Link } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Home
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" to={"/"}>
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to={"/about"}>
+                  About
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default App;
